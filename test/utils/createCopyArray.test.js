56465ebcbcbb5eafafa5efd295db8feec2dcc884
Ant Stanley <antstanley@users.noreply.github.com>
2020-04-20 21:35:57 +0100

    test: add copyArray test. fix copyArray function

--- test/utils/createCopyArray.test.js
+++ test/utils/createCopyArray.test.js
@@ -1,3 +1,17 @@
 import test from 'ava'
+import { join } from 'path'
+import createCopyArray from '../../src/utils/createCopyArray.js'
 
-test.todo('Implement Test')
+test('createCopyArray Test', t => {
+  const inputArray = [join(process.cwd(), './test/sample/function/index.js')]
+  const source = join(process.cwd(), './test/sample/')
+  const dest = join(process.cwd(), './dest/')
+  const expectedResult = [
+    {
+      source: join(process.cwd(), './test/sample/function/index.js'),
+      dest: join(process.cwd(), './dest/function/index.js')
+    }
+  ]
+  const copyArrayResult = createCopyArray(source, inputArray, dest)
+  t.deepEqual(copyArrayResult, expectedResult, 'message')
+})
